1b3ff9e4814682788294e6ef5f818ed5288a4a0d
AddAsher <asheram2013@gmail.com>
2026-02-12 00:11:39 -0500

    22-Objects and Classes

--- code/02 JS Refresher/code/finished/assets/scripts/app.js
+++ code/02 JS Refresher/code/finished/assets/scripts/app.js
@@ -12,13 +12,45 @@ import * as util from "./util.js";
 
 // console.log(userMessage);
 
-function createGreeting(userName, message){
-    // console.log('Hello ' + userName + '! ' + message);
-    return "Hi, I am " + userName + ". " + message;
-}
+// function createGreeting(userName, message){
+//     // console.log('Hello ' + userName + '! ' + message);
+//     return "Hi, I am " + userName + ". " + message;
+// }
 
-createGreeting("Max", "How are you doing?");
-console.log(createGreeting("Max", "How are you doing?"));
+// createGreeting("Max", "How are you doing?");
+// console.log(createGreeting("Max", "How are you doing?"));
+// export default (userName, message) => {
+//     console.log('Hello');
+//     return userName + message;
+// }
+
+// Objects and Classes
+// const user = {
+//     name: "Max",
+//     age: 34,
+//     greet(){
+//         console.log('Hello!');
+//         console.log(this.age);
+//     }
+// };
+
+// console.log(user);
+// user.greet();
+
+// class User {
+//     constructor(name, age){
+//         this.name = name;
+//         this.age = age;
+//     }
+    
+//     greet(){
+//         console.log('Hi! I am ' + this.name);
+//     }
+// }
+
+// const user1 = new User("Manuel", 35);
+// console.log(user1);
+// user1.greet();
 
 //Completed Code section
 /*//  import { apiKey } from "./util.js";
