6f5252435b894f36711536fce5cee170050d8d4d
AddAsher <asheram2013@gmail.com>
2026-03-09 22:47:22 -0400

    Making components reusable with Props

--- code/03 React Essentials/00-MyCode/Mycode/src/App.jsx
+++ code/03 React Essentials/00-MyCode/Mycode/src/App.jsx
@@ -1,7 +1,8 @@
-import './App.css'
-import reactImg from './assets/react-core-concepts.png';
+import "./App.css";
+import reactImg from "./assets/react-core-concepts.png";
+import componentsImg from './assets/components.png';
 
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
@@ -22,12 +23,33 @@ function Header() {
   );
 }
 
+function CoreConecpt(props) {
+  return (
+    <li>
+      <img src={props.imgSrc} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConecpt
+              title="Components"
+              description="the core UI building block."
+              imgSrc={componentsImg}
+            />
+            <CoreConecpt title = "Props"/>
+            <CoreConecpt />
+          </ul>
+        </section>
       </main>
     </div>
   );
