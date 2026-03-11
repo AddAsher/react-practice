e6d3da32a730369550624b35cd472b6f6bda1254
AddAsher <asheram2013@gmail.com>
2026-03-11 17:50:55 -0400

    Alternativve Prop Syntaxes

--- code/03 React Essentials/00-MyCode/Mycode/src/App.jsx
+++ code/03 React Essentials/00-MyCode/Mycode/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import reactImg from "./assets/react-core-concepts.png";
 import componentsImg from './assets/components.png';
+import { CORE_CONCEPTS } from "./data";
 
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
@@ -23,12 +24,22 @@ function Header() {
   );
 }
 
-function CoreConecpt(props) {
+// function CoreConecpt(props) {
+//   return (
+//     <li>
+//       <img src={props.image} alt={props.title} />
+//       <h3>{props.title}</h3>
+//       <p>{props.description}</p>
+//     </li>
+//   );
+// }
+
+function CoreConecpt({ image, title, description }) {
   return (
     <li>
-      <img src={props.imgSrc} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
     </li>
   );
 }
@@ -42,12 +53,13 @@ function App() {
           <h2>Core Concepts</h2>
           <ul>
             <CoreConecpt
-              title="Components"
-              description="the core UI building block."
-              imgSrc={componentsImg}
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
             />
-            <CoreConecpt title = "Props"/>
-            <CoreConecpt />
+            <CoreConecpt {...CORE_CONCEPTS[1]} />
+            <CoreConecpt {...CORE_CONCEPTS[2]} />
+            <CoreConecpt {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
       </main>
