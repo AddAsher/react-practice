a04ee39623bb1a2529f9891d840f8cfb68088c4d
AddAsher <asheram2013@gmail.com>
2026-03-31 21:41:19 -0400

    51 - passing functions as values to props

--- code/03 React Essentials/00-MyCode/Mycode/src/components/TabButton.jsx
+++ code/03 React Essentials/00-MyCode/Mycode/src/components/TabButton.jsx
@@ -1,10 +1,11 @@
-export default function TabButton({children}) {
-  function handleClick() {
-    console.log(`You clicked on the ${children} tab!`);
-  }
+export default function TabButton({children, onSelect}) {
+  // function handleClick() {
+  //   console.log(`You clicked on the ${children} tab!`);
+  // }
+
   return (
     <li>
-      <button onClick={handleClick}>{children}</button>
+      <button onClick={onSelect}>{children}</button>
     </li>
   );
 //   return (
