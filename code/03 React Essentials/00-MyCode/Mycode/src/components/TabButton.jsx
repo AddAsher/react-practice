4f8d998a7f7d591744eaf736eb6005fc5fbb6efc
AddAsher <asheram2013@gmail.com>
2026-04-13 17:40:15 -0400

    56 - CSS Styling & Dynamic Styling

--- code/03 React Essentials/00-MyCode/Mycode/src/components/TabButton.jsx
+++ code/03 React Essentials/00-MyCode/Mycode/src/components/TabButton.jsx
@@ -1,11 +1,13 @@
-export default function TabButton({children, onSelect}) {
+export default function TabButton({children, onSelect, isSelected}) {
   // function handleClick() {
   //   console.log(`You clicked on the ${children} tab!`);
   // }
 
   return (
     <li>
-      <button onClick={onSelect}>{children}</button>
+      <button className={isSelected && 'active' } onClick={onSelect}>
+        {children}
+      </button>
     </li>
   );
 //   return (
