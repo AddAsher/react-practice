197d8d84bb3f266191afdf046cf58312b16357cd
AddAsher <asheram2013@gmail.com>
2026-04-16 17:10:58 -0400

    81 - Updating Object State Immutably

--- code/04 Essentials Deep Dive/00-MyCode/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ code/04 Essentials Deep Dive/00-MyCode/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
@@ -5,14 +7,29 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+        // we do this because even though we are updating the state right,
+        // the object reference of the game board is still the same,
+        //  so react doesn't know that it needs to re-render.
+        //  (value changed but no visual changed as it is the same reference)
+        const updatedGameBoard = [...prevGameBoard.map(innerArray => [...innerArray])]; 
+        updatedGameBoard[rowIndex][colIndex] = "X";
+        return updatedGameBoard;
+        
+    });
+  }
+
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick = {() => handleSelectSquare(rowIndex,colIndex)}>{playerSymbol}</button>
               </li>
             ))}
           </ol>
