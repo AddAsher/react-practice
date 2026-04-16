7314701ad797eeec784698f422c3b1a2263664f2
AddAsher <asheram2013@gmail.com>
2026-04-16 17:22:31 -0400

    82 - Lifting State up

--- code/04 Essentials Deep Dive/00-MyCode/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ code/04 Essentials Deep Dive/00-MyCode/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -6,20 +6,23 @@ const initialGameBoard = [
   [null, null, null],
 ];
 
-export default function GameBoard() {
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
   const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
   function handleSelectSquare(rowIndex, colIndex) {
     setGameBoard((prevGameBoard) => {
-        // we do this because even though we are updating the state right,
-        // the object reference of the game board is still the same,
-        //  so react doesn't know that it needs to re-render.
-        //  (value changed but no visual changed as it is the same reference)
-        const updatedGameBoard = [...prevGameBoard.map(innerArray => [...innerArray])]; 
-        updatedGameBoard[rowIndex][colIndex] = "X";
-        return updatedGameBoard;
-        
+      // we do this because even though we are updating the state right,
+      // the object reference of the game board is still the same,
+      //  so react doesn't know that it needs to re-render.
+      //  (value changed but no visual changed as it is the same reference)
+      const updatedGameBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol;
+      return updatedGameBoard;
     });
+
+    onSelectSquare();
   }
 
   return (
@@ -29,7 +32,9 @@ export default function GameBoard() {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick = {() => handleSelectSquare(rowIndex,colIndex)}>{playerSymbol}</button>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
