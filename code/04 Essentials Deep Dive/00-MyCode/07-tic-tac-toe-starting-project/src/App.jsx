896fabd2ae335543c18258ae5607574fdd7a4a7a
AddAsher <asheram2013@gmail.com>
2026-04-28 12:23:24 -0400

    83 - Avoiding Intersecting States

--- code/04 Essentials Deep Dive/00-MyCode/07-tic-tac-toe-starting-project/src/App.jsx
+++ code/04 Essentials Deep Dive/00-MyCode/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,12 +1,21 @@
-import Player from './components/Player';
-import GameBoard from './components/GameBoard';
 import {useState } from 'react';
 
+import GameBoard from './components/GameBoard';
+import Player from './components/Player';
+import Log from './components/Log';
+
 function App() {
+  const [gameTurns, setGameTurns] = useState([]);
   const [activePlayer, setActivePlayer] = useState('X');
 
   function handleSelectSquare(rowIndex, colIndex) {
+    //here we are using the callback form of the state setter function 
+    // because we are updating the state based on the previous state, 
+    // and this ensures that we are always working with the most
+    //  up-to-date state value,
+    //  even if there are multiple state updates queued up. (preventing stale state issues))
     setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+    
   }
 
   return (
@@ -18,7 +27,7 @@ function App() {
         </ol>
         <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
       </div>
-      Log
+      <Log />
     </main>
   );
 }
